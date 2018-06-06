6a53add3da98b17427b52bf9595d3e0718391bcc
muted.spectre <muted.spectre@gmail.com>
2018-06-06 19:39:45 +0300

    1-е задание.

---
+++ task1.js
@@ -0,0 +1,5 @@
+var numberForTransport, numberForOut ;
+
+numberForTransport = parseInt(process.argv[2], process.argv[3]);
+numberForOut = (numberForTransport).toString(process.argv[4]);
+console.log(numberForOut);
